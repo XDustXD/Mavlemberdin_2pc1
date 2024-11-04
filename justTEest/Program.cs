fdc6c1c2dc98798661579623452bdf3dfb94171a
Danil Mavlemberdin <xdustxd@gmail.com>
2024-11-04 19:32:02 +0500

    The first triangle

--- justTEest/Program.cs
+++ justTEest/Program.cs
@@ -10,6 +10,17 @@ namespace justTEest
     {
         static void Main(string[] args)
         {
+            int height = 5;
+
+            for (int j = 1; j <= height; j++)
+            {
+                for (int k = 1; k <= j; k++)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
         }
     }
 }
